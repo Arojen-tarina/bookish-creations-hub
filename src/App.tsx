8ef05a2765414c755c217e5bda70d4dcf7025af4
Koalabear101 <juusohonkonen2004@gmail.com>
2026-05-24 21:30:44 +0000

    add ads

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Digipeli from "./pages/Digipeli";
 import Ohjekirja from "./pages/Ohjekirja";
+import { HouseAd } from "@/components/ui/HouseAd.tsx";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,11 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
       <Toaster />
       <Sonner />
+      <div className="fixed bottom-0 left-0 right-0 z-40 p-3 pointer-events-auto flex justify-center">
+        <div className="max-w-6xl w-full px-4">
+          <HouseAd slot="global_bottom" variant="banner" className="w-full" />
+        </div>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Digipeli />} />
