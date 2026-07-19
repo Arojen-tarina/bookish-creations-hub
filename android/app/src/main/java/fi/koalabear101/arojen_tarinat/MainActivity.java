67beff54a38c64192e6199ac108313378e98114a
Koalabear101 <juusohonkonen2004@gmail.com>
2026-07-19 06:35:11 +0000

    lovable syncinc, html, poistetaan turhia tiedostoja.

--- android/app/src/main/java/fi/koalabear101/arojen_tarinat/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package fi.koalabear101.arojen_tarinat;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- remotion/src/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { loadFont as loadCinzel } from "@remotion/google-fonts/Cinzel";
-import { loadFont as loadInter } from "@remotion/google-fonts/Inter";
-
-export const cinzel = loadCinzel("normal", { weights: ["600", "700", "900"] }).fontFamily;
-export const inter = loadInter("normal", { weights: ["400", "600", "700"] }).fontFamily;
