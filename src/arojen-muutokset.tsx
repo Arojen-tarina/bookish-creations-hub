921574c6691e48af47f30332429716e559a09702
Koalabear101 <juusohonkonen2004@gmail.com>
2026-07-20 17:52:25 +0000

    Add Arojen muutokset component

---
+++ src/arojen-muutokset.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ArojenMuutokset: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold">Arojen muutokset</h2>
+      <p className="mt-2 text-sm text-gray-600">Tämä tiedosto sisältää Arojen muutokset -paikkamerkin.</p>
+    </div>
+  );
+};
+
+export default ArojenMuutokset;
