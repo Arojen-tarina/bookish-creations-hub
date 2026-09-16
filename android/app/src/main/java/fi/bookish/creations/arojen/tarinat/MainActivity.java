7b7c9dd4b779c56114fdba82dfc655dfb4e2ceb9
Koalabear101 <juusohonkonen2004@gmail.com>
2026-09-16 12:33:06 +0000

    R8 optimointia

--- android/app/src/main/java/fi/bookish/creations/arojen/tarinat/MainActivity.java
+++ android/app/src/main/java/fi/bookish/creations/arojen/tarinat/MainActivity.java
@@ -2,4 +2,12 @@ package fi.bookish.creations.arojen.tarinat;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+/**
+ * Android entry point for the game app.
+ *
+ * The class intentionally stays minimal because Capacitor manages the app lifecycle,
+ * plugin setup, and WebView configuration via BridgeActivity.
+ */
+public class MainActivity extends BridgeActivity {
+    // Intentionally empty: all app setup is inherited from BridgeActivity.
+}
